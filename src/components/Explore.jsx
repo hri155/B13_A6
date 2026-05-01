1c9357efe1ae3490bc22d490c4274a2b8bad847d
Shomita Baishya <hridibaishya@gmail.com>
2026-05-01 12:35:38 +0600

    added logo and description in footer part

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,6 +1,6 @@
 const Explore = () => {
     return(
-        <div className=" text-center bg-linear-to-r from-[#4f39f6] via-[#6a5af9] to-[#8b5cf6]">
+        <div className=" text-center bg-linear-to-r from-[#4f39f6] via-[#6a5af9] to-[#8b5cf6] space-y-2">
             <div className="mt-20 text-center">
                 <h1 className="text-3xl text-white font-bold py-10">Ready to Transform Your Workflow?</h1>
                 <p className="text-gray-200 mb-2">Join thousands of professionals who are already using Digitools to work smarter.<br/>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,39 @@
 const Footer = () => {
     return(
-        <div></div>
+        <div className=" bg-[#101727] px-16 py-10">
+        <footer className="footer sm:footer-horizontal text-base-content ">
+    <aside>
+       <h1 className="text-2xl text-white font-bold py-4">DigiTools</h1>
+        <p className="text-white">
+        Premium digital tools for creators, <br/>
+        professionals, and businesses. Work smarter <br/>
+         with our suite of powerful tools.
+        </p>
+    </aside>
+    <nav>
+        <h6 className="footer-title">Services</h6>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+    </nav>
+    <nav>
+        <h6 className="footer-title">Company</h6>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+    </nav>
+    <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+    </nav>
+    </footer>
+            
+ </div>
 
-    )
-}
+ );
+};
 export default Footer;
