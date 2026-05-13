ac5475594794d6f93952887cc88a377f735cf2b2
Shomita Baishya <hridibaishya@gmail.com>
2026-05-13 20:50:22 +0600

    Edited Build settings

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/DigiTools_demo_UI/',
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
